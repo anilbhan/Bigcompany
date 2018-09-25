f87219a13338bad22cc6984fb838b9ddadd613dd
Anil Bhandari <Bhandarianil921@gmail.com>
2018-09-24 19:14:21 -0700

    Found the duplicate strings.

---
+++ HelloConsole/RemoveDuplicate.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HelloConsole
+//{
+//    class RemoveDuplicate
+//    {
+//        public static void Main()
+//        {
+
+//            string value1 = RemoveDuplicateCharacters("Yahoo");
+//            string value2 = RemoveDuplicateCharacters("CNN");
+//            string value3 = RemoveDuplicateCharacters("Twitter");
+//            string value4 = RemoveDuplicateCharacters("FACEBOOK");
+//            string value5 = RemoveDuplicateCharacters("EYES");
+
+
+//            Console.WriteLine(value1);
+//            Console.WriteLine(value2);
+//            Console.WriteLine(value3);
+//            Console.WriteLine(value4);
+//            Console.WriteLine(value5);
+
+
+//        }
+
+
+
+
+//        public static string RemoveDuplicateCharacters(string key)
+//        {
+//            //store the repeated char in this string;
+//            string table = "";
+
+//            //store the result in this string;
+//            string result = "";
+
+//            //loop over each character
+
+//            foreach (char value in key)
+//            {
+//                //check if the char are in the table
+//                if (table.IndexOf(value) == -1)
+//                {
+//                    //append to the table
+//                    table += value;
+//                    result += value;
+//                }
+//            }
+
+//            return result;
+//        }
+//    }
+//}
---
+++ HelloConsole/RemoveDupscs.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HelloConsole
+//{
+//    class RemoveDupscs
+//    {
+//        public static void Main()
+//        {
+//            //define the variables with repeated string;
+
+//            string Value1 = RemoveDuplicate("Yahoo");
+//            string Value2 = RemoveDuplicate("CNN");
+//            string Value3 = RemoveDuplicate("Google");
+
+//            Console.WriteLine(Value1);
+//            Console.WriteLine(Value2);
+//            Console.WriteLine(Value3);
+//        }
+
+//        public static string RemoveDuplicate(string val)
+//        {
+//            //temporary variable to hold repeated character;
+
+//            string table = "";
+//            //to hold result after removing repeated char;
+//            string result = "";
+//            foreach (char value in val)
+//            {
+//                if (table.IndexOf(value) == -1)
+//                {
+//                    table += value;
+//                    result += value;
+
+//                }
+//            }
+//            return result;
+//        }
+//    }
+//}
